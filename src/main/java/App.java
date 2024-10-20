2206be84e8d88de7051d5001f52837003953a0cb
gyoungsubLee <dltjq2323@gmail.com>
2024-10-20 20:35:00 +0900

    ♻️ refactor/App: 유사한 동작을 가진 메서드 오버로딩 사용 • performFirstCalculation, performAdditionalCalculation     => performCalculation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ import calculator.lv2.repository.CalculationNumberRepositoryImp;
 import java.util.Scanner;
 
 
-public class Test {
+public class App {
     // 각 타입 별로 입력 메세지 및 입력값 받아오는 메서드
     public static <T>T input(String message, Scanner sc, InputEnum type){
         System.out.print(message);
@@ -19,11 +19,18 @@ public class Test {
     }
 
     // 첫번째 계산을 수행하는 메서드
-    public static double performFirstCalculation(Scanner sc, Calculator calculator){
+    public static double performCalculation(Scanner sc, Calculator calculator){
         double result = input("첫 번째 숫자를 입력하세요:", sc, InputEnum.NUMBER);
         char operator = input("사칙연산 기호를 입력하세요: ", sc, InputEnum.OPERATOR);
         double num2 = input("두 번째 숫자를 입력하세요:", sc, InputEnum.NUMBER);
         return  calculator.calculate(operator, result, num2);
+    }
+
+    // 추가 계산을 수행하는 메서드, 오버로딩 사용
+    public static double performCalculation(Scanner sc, Calculator calculator, double result) {
+        double repeatNum = input("계산을 추가할 숫자를 입력하세요:", sc, InputEnum.NUMBER);
+        char repeatOperator = input("추가 사칙연산 기호를 입력하세요: ", sc, InputEnum.OPERATOR);
+        return calculator.calculate(repeatOperator, result, repeatNum);
     }
 
     // 추가 계산 여부 판단 메서드
@@ -32,12 +39,7 @@ public class Test {
         return !continueInput.equalsIgnoreCase("NO");  // 대소문자 구분 X
     }
 
-    // 추가 계산을 수행하는 메서드
-    public static double performAdditionalCalculation(Scanner sc, Calculator calculator, double result) {
-        double repeatNum = input("계산을 추가할 숫자를 입력하세요:", sc, InputEnum.NUMBER);
-        char repeatOperator = input("추가 사칙연산 기호를 입력하세요: ", sc, InputEnum.OPERATOR);
-        return calculator.calculate(repeatOperator, result, repeatNum);
-    }
+
 
 
     public static void  main(String[] args) {
@@ -46,10 +48,11 @@ public class Test {
         Calculator calculator = new CalculatorImp(new OperationAdapter(), new CalculationNumberRepositoryImp());
 
         // 첫 번째 계산 수행
-        double result = performFirstCalculation(sc, calculator);
+        double result = performCalculation(sc, calculator);
+
         // 추가 계산 여부 확인 및 반복 수행
         while (shouldContinue(sc)) {
-            result = performAdditionalCalculation(sc, calculator, result);
+            result = performCalculation(sc, calculator, result);
         }
 
         System.out.println("최종 결과 값 = " + result);
